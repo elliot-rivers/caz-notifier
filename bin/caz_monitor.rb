c9d17bd9c42cb86b2db003ee1b5f428a47ea4a79
Elliot Rivers <erivers@onemedical.com>
2024-10-01 17:31:11 -0500

    beh i don't need synch

--- bin/caz_monitor.rb
+++ bin/caz_monitor.rb
@@ -3,9 +3,5 @@ require_relative '../lib/caz/monitor'
 require_relative '../lib/caz/orchestrator'
 require_relative '../lib/caz/notifier'
 
-require 'io/console'
-$stdout.sync = true
-
-
 orchestrator = Caz::Orchestrator.new
 orchestrator.run
